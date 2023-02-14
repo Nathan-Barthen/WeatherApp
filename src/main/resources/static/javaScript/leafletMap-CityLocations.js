7a128cd7932776c01219b2c7880bfad1b4f660e0
Narthen <narthen34@gmail.com>
2023-02-14 17:12:40 -0500

    did things with Maps

--- src/main/resources/static/javaScript/leafletMap-CityLocations.js
+++ src/main/resources/static/javaScript/leafletMap-CityLocations.js
@@ -5,6 +5,20 @@
  *   These locations will be displayed on a map where the user can select the correct location
  */
 		
+	//Changes the color of the iconImg on the at the top of the webpage (not on the map)
+		const colors = ["blue", "yellow", "green", "orange", "violet"];
+		const divs = document.querySelectorAll("#top-locationContainer");
+		
+		for (let i = 0; i < divs.length; i++) {
+		  const div = divs[i];
+		  const iconImg = div.querySelector("#iconImgDiv img");
+		  const colorIndex = i % colors.length;
+		  const colorVar = colors[colorIndex];
+		  iconImg.src = "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-" + colorVar + ".png";
+		}
+
+
+	//Gets the middle of the screen by getting the mean of the lon and lat values
 		var sumLat = 0;
 		var sumLon = 0;
 		var topLocations = document.querySelectorAll('#top-location');
@@ -21,17 +35,30 @@
 		var centerLat = sumLat / topLocations.length;
 		var centerLon = sumLon / topLocations.length;
 
+  //Creates the leaflet map
 		var map = L.map('mapid').setView([centerLat, centerLon], 7); // initialize the map and set the view to the center of the state
 	  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	      attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
 	  }).addTo(map); // add the tile layer
 	  
 	  
-	  
+ //Adds each marker for each location.  
 	  //Open popups for each marker
 	  for (var i = 0; i < topLocations.length; i++) {
+		  var cityElement = document.querySelector('#city');
+		  var city = cityElement.textContent;
 		  var topLocation = topLocations[i];
 		  var lat = parseFloat(topLocation.textContent.match(/Lat: ([-\d.]+)/)[1]);
 		  var lon = parseFloat(topLocation.textContent.match(/Lon: ([-\d.]+)/)[1]);
-		  var marker = L.marker([lat, lon], {title: "Location " + (i + 1)}).addTo(map);
+		  var icon = new L.Icon({
+			  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-' +  colors[i] +'.png',
+			  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+			  iconSize: [25, 41],
+			  iconAnchor: [12, 41],
+			  popupAnchor: [1, -34],
+			  shadowSize: [41, 41]
+			});
+		  
+		  var marker = L.marker([lat, lon], {icon: icon} ).addTo(map);
+		  marker.bindPopup(city);
 		}
---
+++ src/main/resources/static/javaScript/leafletMap-PrecipitationMap.js
@@ -0,0 +1,41 @@
+/**
+ * Used to create the leaflet map to show city locations on chooseCorrectLocation.html
+ * 	Used when user searches using City+State
+ * 	The openWeather API returns a list of locations
+ *   These locations will be displayed on a map where the user can select the correct location
+ */
+		
+		var lat = parseFloat(document.getElementById("lat").value);
+		var lon = parseFloat(document.getElementById("lon").value);
+		var apiKey = document.getElementById("apiKey").value;
+		var city = document.getElementById("city").value
+		console.log("Hello- " + lat + " " + lon + " " + apiKey);
+		
+		
+	    var map = L.map("precipitationMap").setView([lat, lon], 7);
+
+	    //Add background map to show city/states/counties
+	    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+	    	  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	    	}).addTo(map);
+	    
+	    var marker = L.marker([lat, lon]).addTo(map);
+		  marker.bindPopup(city);
+		
+		//State outline layer
+	
+		  
+	    //Add openWeather precipitation map
+		L.tileLayer("https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=" + apiKey, {
+		  maxZoom: 18,
+		  attribution: 'Map data © OpenWeatherMap'
+		}).addTo(map);
+		
+		//Add openWeather clouds map
+		L.tileLayer("https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=" + apiKey, {
+			  maxZoom: 18,
+			  attribution: 'Map data © OpenWeatherMap'
+			}).addTo(map);
+		
+		
+		
