200c23b60c803f6f668a3a1bcabc2186013c62cd
Narthen <narthen34@gmail.com>
2023-02-17 13:27:34 -0500

    worked on weatherScreen Today page and api

--- src/main/java/indp/nbarthen/proj/repository/TodayReport.java
+++ src/main/java/indp/nbarthen/proj/repository/TodayReport.java
@@ -9,7 +9,9 @@ import jakarta.persistence.OneToOne;
 import java.util.Date;
 import java.util.Vector;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.ibm.icu.text.SimpleDateFormat;
+import com.ibm.icu.util.TimeZone;
 
 /*
  * Today:
@@ -32,17 +34,18 @@ public class TodayReport {
 	private double currTemp;
 	private double currFeelsLike;
 	private int currHumidity; 		// as a % (0-100)
+	private int visibility;			//meters (max is 10km)
 	
 	private String downfallType; 	//Optional: Ex. 'Rain' 'Snow;
-	private double downfallAmount; 	//Optional: Ex. ' "1h": 3.16 ' Could be '1h' or '3h'
+	private double downfallAmount; 	//Optional: Ex. ' "1h": 3.16 ' Could be '1h' or '3h' (in mm)
 	
 	private double windSpeed;		// m/s (meters per second)
 	private int cloudiness; 		// as a % (0-100)
 	
-	private int sunrise; 			// time, unix, UTC
-	private int sunset; 			// time, unix, UTC
+	private int sunrise; 			// time, unix (seconds), UTC 
+	private int sunset; 			// time, unix (seconds), UTC
 	
-	private int time; 		//Time of data calculation, unix, UTC
+	private int time; 		//Time of data calculation, unix (seconds), UTC 
 	private int timezone; 	//Shift in seconds from UTC
 	
 	private int cityPopulation;
@@ -136,6 +139,20 @@ public class TodayReport {
 		this.currHumidity = currHumidity;
 	}
 
+	@JsonIgnore
+	public String getVisibilityMiles() {
+		//Returns visibility in miles (rounded to 1 decimal place)
+		double visibilityInMiles = visibility * 0.000621;
+		visibilityInMiles = Math.round(visibilityInMiles * 10.0) / 10.0;
+		return String.format("%.1f", visibilityInMiles);
+	}
+	public int getVisibility() {
+		return visibility;
+	}
+
+	public void setVisibility(int visibility) {
+		this.visibility = visibility;
+	}
 
 	public String getDownfallType() {
 		return downfallType;
@@ -147,6 +164,14 @@ public class TodayReport {
 	}
 
 
+	@JsonIgnore
+	public String getDownfallAmountMmAndInches() {
+		double downfallInches = downfallAmount / 0.0393701; // conversion factor: 1 mm = 0.0393701 inches
+		downfallInches = Math.round(downfallInches * 100.0) / 100.0; // round to 2 decimal places
+		
+		return Double.toString(downfallInches) + "in (" + Double.toString(downfallAmount) + "mm)";
+		
+	}
 	public double getDownfallAmount() {
 		return downfallAmount;
 	}
@@ -179,11 +204,21 @@ public class TodayReport {
 	}
 
 
-	public String getSunrise() {
-		//Converts Sunrise to local time (ex. 6:21am)
-		SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
-		 
-		return timeFormat.format(sunrise).toLowerCase();
+	@JsonIgnore
+	public String getSunriseTime() {
+		// Convert sunrise time to milliseconds
+        long sunriseMillis = (sunrise+timezone) * 1000L;
+        
+        // Set the time zone to UTC
+        SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
+        timeFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        
+        // Format the sunrise time to h:mm a format in UTC time zone
+        return timeFormat.format(new Date(sunriseMillis)).toLowerCase();
+	}
+	
+	public int getSunrise() {
+		return sunrise;
 	}
 
 
@@ -192,6 +227,18 @@ public class TodayReport {
 	}
 
 
+	@JsonIgnore
+	public String getSunsetTime() {
+		// Convert sunrise time to milliseconds
+        long sunsetMillis = (sunset+timezone) * 1000L;
+        
+        // Set the time zone to UTC
+        SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
+        timeFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        
+        // Format the sunrise time to h:mm a format in UTC time zone
+        return timeFormat.format(new Date(sunsetMillis)).toLowerCase();
+	}
 	public int getSunset() {
 		return sunset;
 	}
@@ -231,7 +278,58 @@ public class TodayReport {
 		this.cityPopulation = cityPopulation;
 	}
 
-
+	
+	@JsonIgnore
+	public String getSolarNoon() {
+		// Calc solar noon time to seconds
+		long solarNoon = (sunrise + sunset) / 2;
+		//convert to miliseconds
+        long solarNoonMillis = solarNoon * 1000L;
+        
+        // Set the time zone to UTC
+        SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
+        timeFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        
+        // Format the sunrise time to h:mm a format in UTC time zone
+        return timeFormat.format(new Date(solarNoonMillis)).toLowerCase();
+	}
+	
+	@JsonIgnore
+	public String getTotalSunlight() {
+		// Calc total sunlight in the day
+		long sunlight = sunset-sunrise;
+		
+		int hours = (int) (sunlight / 3600);
+		int minutes = (int) ((sunlight % 3600) / 60);
+		
+		return hours + "hr " + minutes + "min";
+        
+	}
+	
+	@JsonIgnore
+	public String getRemainingSunlight() {
+		long timezoneMilli = timezone * 1000L;
+		//Get current time in users timezone (milliseconds)
+		long currentUtcTime = ( System.currentTimeMillis() );
+		// Get sunset time (milliseconds)
+		long sunsetMillis = (sunset) * 1000L;
+		
+		//Gets sunlightLeft adding in users timezone shift
+		long sunlightLeft = sunsetMillis - currentUtcTime;
+		
+		long sunlightLeftSeconds = sunlightLeft / 1000;
+		int hours = (int) (sunlightLeftSeconds / 3600);
+		int minutes = (int) ((sunlightLeftSeconds % 3600) / 60);
+		
+		if(sunlightLeft <= 0) {
+			return "None";
+		}
+		
+		return hours + "hr " + minutes + "min";
+        
+	}
+	
+	
 		public String getApiError() {
 			return apiError;
 		}
